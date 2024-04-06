2bbf7ef02c5cf745bf253f87faec56c5270aa081
IngJarolGarzon <166014529+IngJarolGarzon@users.noreply.github.com>
2024-04-05 23:42:10 -0500

    Add files via upload

---
+++ C++/IntroctionClassesC++/PrimerosProgramas/Practca de get,set c++.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+class MyClass {
+public:int x;
+
+private:int y;  
+
+public:void setY(int s) 
+{
+	y = s;
+}
+int gety() {
+	return y;
+}
+};
+
+int main() {MyClass myObj;
+myObj.x = 1;
+myObj.setY(2);
+cout<< "Este es x = " << myObj.x << endl;
+cout<< "Este es y = " << myObj.gety() << endl;
+return 0;
+}
