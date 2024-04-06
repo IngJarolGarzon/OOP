9a388e53db9e16d1f9698e624c0757ff3f258fbf
IngJarolGarzon <166014529+IngJarolGarzon@users.noreply.github.com>
2024-04-06 01:08:37 -0500

    Add files via upload

---
+++ Java/GraphicInterface/IntroductionInter/Interface1.java
@@ -0,0 +1,15 @@
+package interfaze1;
+
+import javax.swing.JOptionPane;
+
+public class Interface1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog (null, "welcome\nto\nJava\nPrograming!");
+        System.exit(0);
+    }
+    
+}
